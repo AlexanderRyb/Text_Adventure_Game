2bc92ee3e766afa354743fc9e867e0d4b4414d10
Alexander <AlexanderRy@protonmail.com>
2021-11-17 20:17:50 +0200

    added redux

---
+++ src/redux/reducer.ts
@@ -0,0 +1,10 @@
+const initialState = {
+    
+}
+const Reducer =(state=initialState, action:any)=>{
+    switch(action.type){
+        case "START":
+            return state
+    }
+}
+export default Reducer;
